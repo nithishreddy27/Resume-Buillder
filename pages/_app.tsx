725eef6a3cfbd10430d0bb6cdfd9807e794f7f99
nithishreddy27 <nithishreddy0627@gmail.com>
2023-02-07 17:42:06 +0530

    nr added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import ResumeState from "../pages/context/ResumeState"
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
-    <ResumeState>
       <Component {...pageProps} />
-    </ResumeState>
   )
 }
