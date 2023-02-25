b22ae7445070d1e60d4e3045925c0fae7b5cc790
unknown <sakshivuppalanchi@gmail.com>
2023-02-25 11:08:51 +0530

    sak

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,5 +1,7 @@
 import React, { useState, useContext } from "react";
+
 import ResumeContext from "../context/ResumeContext";
+
 import {
   AiOutlineCaretDown,
   AiOutlineCaretUp,
@@ -10,6 +12,7 @@ import { CgProfile } from "react-icons/cg";
 import { MdSocialDistance, MdOutlineSpeakerNotes } from "react-icons/md";
 import { FaLanguage, FaAward } from "react-icons/fa";
 import { SiGooglescholar } from "react-icons/si";
+import {FaEdit} from "react-icons/fa"
 // import {GrProjects} from "react-icons/gr"
 import { GiSkills } from "react-icons/gi";
 import { RxHobbyKnife } from "react-icons/rx";
@@ -27,6 +30,7 @@ export default function SideBar() {
   const [parrow, setparrow] = useState(false);
   const [awarrow, setawarrow] = useState(false);
   const [carrow, setcarrow] = useState(false);
+  
   const { details, setdetails } = useContext(ResumeContext);
   const [editSocial, seteditSocial] = useState(false);
 
@@ -646,9 +650,22 @@ export default function SideBar() {
                           <span className="px-2 mt-[-3px] font-semibold grow">
                             {item.network}
                           </span>
-                          <button onClick={() => deleteSocialNetwork(index)}>
+                          <div className="text-xl mr-1 mt-[2px] text-gray-300">
+                            <FaEdit></FaEdit>
+                          </div>
+                          <button className="text-xl mr-2 text-gray-300" onClick={() => deleteSocialNetwork(index)}>
                             <AiFillDelete></AiFillDelete>
                           </button>
+                          <div>
+    <input
+      class="mt-[0.3rem] mr-2 h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-gray-300 outline-none before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-white after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-orange-600 checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
+      type="checkbox"
+      role="switch"
+      id="flexSwitchCheckDefault" />
+    
+  </div>
+                          
+                          
                           {/* <button onClick={()=>updateSocialNetwork(index)}>Update</button> */}
                         </div>
                         <div className="block text-sm font-thin">
