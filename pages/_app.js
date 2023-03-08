6fea11d4cc75c222b3a18259b6a273b392605b3c
rubeena khatun <rubeenakhatun112@gmail.com>
2023-03-08 13:38:57 +0530

    loading icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,32 @@
-import '../styles/globals.css'
-
-import ResumeState from "../context/ResumeState"
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
+import Router from "next/router";
+import Loading from "../components/Loading";
+import ResumeState from "../context/ResumeState";
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
 
-  return <>
-    <ResumeState>
-      <Component {...pageProps} />
-    </ResumeState>
-  </>
+  // Listen for route changes to show/hide the loading component
 
+  Router.events.on("routeChangeStart", (url) => {
+    console.log("Loading start");
+    setLoading(true);
+  });
+  Router.events.on("routeChangeComplete", (url) => {
+    console.log("Loading end");
+    setLoading(false);
+  });
+  // Router.events.on("routeChangeError", handleComplete);
+
+  return (
+    <>
+      <ResumeState>
+        {loading && <Loading />}
+        <Component {...pageProps} />
+      </ResumeState>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
